a5404bafae2e7fda13c1f78aac7025a115f19ad1
Jan Decaluwe <jan@jandecaluwe.com>
2015-12-29 10:20:22 +0100

    migrate to Urubu 1.0

--- _python/__init__.py
+++ _python/__init__.py
@@ -1,2 +1,14 @@
 from .filters import filters
-from .validators import validators
+
+from distutils.version import StrictVersion
+import urubu
+
+urubu_version_required = "1.0"
+
+urubu_version_error = """\
+Urubu version should be >= {}
+Upgrade with: "pip install --upgrade urubu"
+""".format(urubu_version_required)
+
+if StrictVersion(urubu.__version__) < StrictVersion(urubu_version_required):
+    raise AssertionError(urubu_version_error) 
